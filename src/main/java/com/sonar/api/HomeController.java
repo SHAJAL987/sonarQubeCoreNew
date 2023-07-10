228e948e3c033da8051f3a95f22df33019ce9c0b
20230357 <ashiqul-shajal@thecitybank.com>
2023-07-10 16:25:56 +0600

    Refactor codebase

---
+++ src/main/java/com/sonar/api/HomeController.java
@@ -0,0 +1,15 @@
+package com.sonar.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sonar")
+public class HomeController {
+
+    @GetMapping("/v1")
+    public String test(){
+        return "Hello";
+    }
+}
