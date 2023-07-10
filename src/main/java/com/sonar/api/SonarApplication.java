2b2a451bd6f08bd9a7e5ef95e53e05ff6a943f4a
20230357 <ashiqul-shajal@thecitybank.com>
2023-07-10 16:19:40 +0600

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/sonar/api/SonarApplication.java
+++ src/main/java/com/sonar/api/SonarApplication.java
@@ -2,11 +2,21 @@ package com.sonar.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/sonar")
 public class SonarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SonarApplication.class, args);
+	}
+
+	@GetMapping("/v1")
+	public String test(){
+		return "Hello";
 	}
 }
